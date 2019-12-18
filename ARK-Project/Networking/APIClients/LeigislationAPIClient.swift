bde00f436e324f0484242023798899af73b7466b
hildyabreu114 <hildyabreu@pursuit.org>
2019-12-18 16:50:56 -0500

    obtain apiKey did appError networkManager and LegislatorAPIClient

--- ARK-Project/Networking/APIClients/LeigislationAPIClient.swift
+++ ARK-Project/Networking/APIClients/LeigislationAPIClient.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+struct LegislatorsAPIClient {
+    private init() {}
+    static let shared = LegislatorsAPIClient()
+    
+    func getMembersInfo(state: String , completionHandler: @escaping (Result<[Member], AppError>) -> ()) {
+        let urlStr = "https://api.propublica.org/congress/v1/116/senate/members.json"
+
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(AppError.badURL))
+            return
+        }
+        NetworkManager.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(.other(rawError: error)))
+                
+            case .success(let data):
+                
+                do {
+                    let congressWomen = try CongressWoman.decodeLegislators(from: data)
+                    completionHandler(.success(congressWomen!))
+                }
+                catch {
+                    completionHandler(.failure(.other(rawError: error)))
+                }
+            }
+        }
+    }
+    
+}
