0377201b7c5a4acf6e3b27c364feac207b450d9a
Aaron Pachesa <aaronpachesa@gmail.com>
2019-12-18 20:50:34 -0500

    flood info model placed

--- ARK-Project/Models/FloodInfo.swift
+++ ARK-Project/Models/FloodInfo.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Place {
+    var latitude: Double
+    var longitude: Double
+    var floodedInYear: Int
+    var directionOfSafeZone: String
+    var feetUnderWater: Int
+    var populationEffected: Int
+    var searchName: String
+    var danger: Int
+}
+var place1 = Place(latitude: 40.7605031, longitude: -73.9509934, floodedInYear: 2200, directionOfSafeZone: "West", feetUnderWater: 7, populationEffected: 11_945, searchName: "Roosevelt Island", danger: 3)
+var place2 = Place(latitude: 25.7825452, longitude: -80.2996701, floodedInYear: 2200, directionOfSafeZone: "West", feetUnderWater: 6, populationEffected: 463_347, searchName: "Miami", danger: 2)
+var place3 = Place(latitude: 36.1251954, longitude: -115.3154268, floodedInYear: 3500, directionOfSafeZone: "SouthWest", feetUnderWater: 6, populationEffected: 641_676, searchName: "Las Vegas", danger: 7)
